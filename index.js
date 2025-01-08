1a0f288ddf762d8c2ae2aca1df0c6c0bb462690d
josdic1 <joshuadicker@gmail.com>
2025-01-08 14:08:50 -0500

    finished lab

---
+++ index.js
@@ -0,0 +1,21 @@
+let a;
+let b;
+let x;
+
+const divide = () => {
+   a = 2000
+   b = 100
+   return Math.abs(a / b);
+}
+
+const square = () => {
+  x = 2
+  return Math.abs(x**x)
+}
+
+
+const add = () => {
+   a = 3
+   b = 4
+   return Math.abs(a + b)
+}
